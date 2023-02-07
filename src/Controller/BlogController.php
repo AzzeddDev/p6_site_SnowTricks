84119dc9c46f562617c875d7d13e51fc9d5ae62a
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2023-02-07 21:58:31 +0100

    update BlogController.php

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,15 +5,8 @@ namespace App\Controller;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
 use App\Entity\Article;
 use App\Form\ArticleType;
 
