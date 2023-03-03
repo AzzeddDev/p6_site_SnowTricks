47197f06fd83373f67b296ab11bf28422d0cb92a
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2023-03-03 19:27:00 +0100

    update entity Article.php

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,7 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -17,11 +20,14 @@ class Article
      */
     private $id;
 
+    //* @Assert\Length(min=10, max=25, minMessage="Votre titre doit contenir minimum 10 charachters") --> for title min length 10 max 25 words
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $title;
 
+
+    //@Assert\Length(min=255) --> for minimum 255 words
     /**
      * @ORM\Column(type="text")
      */
@@ -29,6 +35,7 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Url()
      */
     private $image;
 
@@ -36,6 +43,28 @@ class Article
      * @ORM\Column(type="datetime")
      */
     private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="article", orphanRemoval=true)
+     */
+    private $comments;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Images::class, mappedBy="article")
+     */
+    private $images_id;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+        $this->images_id = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -86,6 +115,78 @@ class Article
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Images>
+     */
+    public function getImagesId(): Collection
+    {
+        return $this->images_id;
+    }
+
+    public function addImagesId(Images $imagesId): self
+    {
+        if (!$this->images_id->contains($imagesId)) {
+            $this->images_id[] = $imagesId;
+            $imagesId->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImagesId(Images $imagesId): self
+    {
+        if ($this->images_id->removeElement($imagesId)) {
+            // set the owning side to null (unless already changed)
+            if ($imagesId->getArticle() === $this) {
+                $imagesId->setArticle(null);
+            }
+        }
 
         return $this;
     }
