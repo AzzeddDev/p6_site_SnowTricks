496fd6c467a510622a03211cc4654e0c2bba8ae0
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2023-04-17 10:35:00 +0200

    add Images Entity

---
+++ src/Entity/Images.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ImagesRepository::class)
+ */
+class Images
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $link;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Article::class, inversedBy="images")
+     */
+    private $article;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getArticle(): ?Article
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?Article $article): self
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+}
