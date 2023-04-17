65e24482037b7ea7c329ffbfcef927faabd86675
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2023-04-17 10:34:28 +0200

    add pagination into home controller

---
+++ src/Controller/Home/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Home;
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(ArticleRepository $repo, Request $request): Response {
+
+        $page = $request->query->get('page');
+        if ($page === null) {
+            $page = 1;
+        }
+        $articles = $repo->findArticlePaginated($page);
+
+        return $this->render('blog/home.html.twig', [
+            'articles' => $articles,
+            'page' => $page,
+        ]);
+    }
+}
