b1d4efd4a566567da9430a8d9c963c549f9f1bbd
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2023-04-17 10:52:50 +0200

    update show tricks controller

--- src/Controller/Tricks/ShowTricksController.php
+++ src/Controller/Tricks/ShowTricksController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Tricks;
 
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShowTricksController extends AbstractController
@@ -17,7 +20,6 @@ class ShowTricksController extends AbstractController
         $articles = $repo->findAll();
 
         return $this->render('blog/blog.html.twig', [
-            'controller_name' => 'BlogController',
             'articles' => $articles
         ]);
     }
